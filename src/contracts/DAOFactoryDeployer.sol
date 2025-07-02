e0c614d2dafce0bd1f1b814879f83df3a0f5b067
Arjun <finaptixapp@gmail.com>
2025-07-02 23:35:47 +0600

    Update DAOFactoryDeployer.sol

--- src/contracts/DAOFactoryDeployer.sol
+++ src/contracts/DAOFactoryDeployer.sol
@@ -2,16 +2,17 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-import "./DAOFactory.sol";
-import "./DAOStorageModule.sol";
-import "./DAODeploymentModule.sol";
-import "./DAOConfigModule.sol";
-import "./DAOEventModule.sol";
 import "./SoulboundIdentityNFT.sol";
+
+interface IAccessControlModule {
+    function DEFAULT_ADMIN_ROLE() external view returns (bytes32);
+    function grantRole(bytes32 role, address account) external;
+    function revokeRole(bytes32 role, address account) external;
+}
 
 /**
  * @title DAOFactoryDeployer
- * @dev Helper contract to deploy DAOFactory with all its modules
+ * @dev Lightweight helper contract for role setup - modules should be deployed separately
  */
 contract DAOFactoryDeployer {
     event DAOFactoryDeployed(
@@ -22,42 +23,39 @@ contract DAOFactoryDeployer {
         address eventModule
     );
     
-    function deployDAOFactory(
-        address _daoImplementation,
-        address _tokenImplementation,
-        SoulboundIdentityNFT _identityNFT
-    ) external returns (DAOFactory factory) {
-        // Deploy modules
-        DAOStorageModule storageModule = new DAOStorageModule();
-        DAODeploymentModule deploymentModule = new DAODeploymentModule(_daoImplementation, _tokenImplementation);
-        DAOConfigModule configModule = new DAOConfigModule();
-        DAOEventModule eventModule = new DAOEventModule();
+    // Setup roles after manual factory deployment
+    function setupRoles(
+        address _factory,
+        address _storageModule,
+        address _deploymentModule,
+        address _configModule,
+        address _eventModule
+    ) external {
+        IAccessControlModule storageModule = IAccessControlModule(_storageModule);
+        IAccessControlModule deploymentModule = IAccessControlModule(_deploymentModule);
+        IAccessControlModule configModule = IAccessControlModule(_configModule);
+        IAccessControlModule eventModule = IAccessControlModule(_eventModule);
         
-        // Deploy factory with module addresses
-        factory = new DAOFactory(
-            _daoImplementation,
-            _tokenImplementation,
-            _identityNFT,
-            address(storageModule),
-            address(deploymentModule),
-            address(configModule),
-            address(eventModule)
-        );
+        bytes32 adminRole = storageModule.DEFAULT_ADMIN_ROLE();
         
-        // Transfer ownership of modules to factory
-        storageModule.transferOwnership(address(factory));
-        deploymentModule.transferOwnership(address(factory));
-        configModule.transferOwnership(address(factory));
-        eventModule.transferOwnership(address(factory));
+        // Grant admin roles to factory
+        storageModule.grantRole(adminRole, _factory);
+        deploymentModule.grantRole(adminRole, _factory);
+        configModule.grantRole(adminRole, _factory);
+        eventModule.grantRole(adminRole, _factory);
+        
+        // Revoke admin roles from this deployer
+        storageModule.revokeRole(adminRole, address(this));
+        deploymentModule.revokeRole(adminRole, address(this));
+        configModule.revokeRole(adminRole, address(this));
+        eventModule.revokeRole(adminRole, address(this));
         
         emit DAOFactoryDeployed(
-            address(factory),
-            address(storageModule),
-            address(deploymentModule),
-            address(configModule),
-            address(eventModule)
+            _factory,
+            _storageModule,
+            _deploymentModule,
+            _configModule,
+            _eventModule
         );
-        
-        return factory;
     }
 }
